7b2ae4d21b0e5d6658b4b0b8ae495987b0437f8f
mateusz-rozkosz-rmtcfm-com <mateusz.rozkosz@remotecraftsmen.com>
2020-12-22 13:56:17 +0100

    comments have been deleted

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,6 @@ app.use(fileUpload());
 
 app.use(express.static('../public'));
 
-//require('./plugins/cors')(app);
-
 const server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
@@ -28,22 +26,5 @@ var io = require('socket.io')(server);
 const routes = require('./routes')(di, io);
 
 app.use(routes);
-
-// const connections = [];
-
-// io.on('connection', (socket) => {
-//     connections.push(socket);
-//     console.log(socket.id);
-
-//     socket.on('disconnect', (data) => {
-//         connections.splice(connections.indexOf(socket), 1);
-//         console.log('disconnected');
-//     });
-
-//     socket.on('message', (data) => {
-//         console.log(data);
-//         io.emit('ios', { msg: 'ddeweewfwefwe' });
-//     });
-// });
 
 module.exports = server;
